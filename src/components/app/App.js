d90239a3641b30b612870c0bce2fd89c125b1250
Pavel E <okis_xxx@mail.ru>
2020-10-27 21:57:28 +0300

    start react project

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   useLocation,
+//   withRouter,
+//   useParams
+// } from "react-router-dom";
+
+class Contacts extends React.Component {
+ 
+  render() {
+
+
+
+    return (
+      <div>
+          hello contacts
+      </div>
+    )
+  }
+}
+export default Contacts;
